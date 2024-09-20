831afd31a73421a3dc7889e0661e5be8128766cd
andrerhenry <kinghenry238@gmail.com>
2024-09-19 22:17:46 -0700

    change specific impulse to properties

--- Rocket_Config.py
+++ Rocket_Config.py
@@ -12,7 +12,7 @@ class RocketConfig:
         self.rocket_mass_0 = rocket_mass_0
         self.drag_coefficient = drag_coefficient
         self._diameter = diameter
-        self._cross_sect_area = self.cross_sect_area_calc(self.diameter)
+        self._cross_sect_area = self.cross_sect_area_calc(diameter)
         pass
     
     @property
@@ -21,7 +21,7 @@ class RocketConfig:
     
     @diameter.setter
     def diameter(self, new_diameter):
-        # Set Cross sectional aera on chagne of diameter
+        # Set Cross sectional aera on change of diameter
         self._cross_sect_area = self.cross_sect_area_calc(new_diameter)
         self._diameter = new_diameter
     
@@ -29,10 +29,10 @@ class RocketConfig:
         """Calcualte the cross sectional area
 
         Args:
-            diameter (float): Diameter of Rocekt - meteres
+            diameter (float): Diameter of Rocket - meters
 
         Returns:
-            float: Cross secontial aera - meters^2
+            float: Cross sectional aera - meters^2
         """        
         return pi*(diameter/2)**2
         
@@ -41,7 +41,7 @@ class Motor():
     GRAVITY = -9.81
     
     def __init__(self, mass_fuel: float, thrust_avg: float, total_impulse: float, total_burn_time: float) -> None:
-        """Inital motor perameters_summary_
+        """Inital motor perameters
 
         Args:
             mass_fuel (float): Mass of fuel to burned - Kg
@@ -49,15 +49,38 @@ class Motor():
             total_impulse (float): Total Impulse  - Newton*seconds
             total_burn_time (float): Burn Time - seconds
         """        
-        self.mass_fuel = mass_fuel
+        self._mass_fuel = mass_fuel
         self.thrust_avg = thrust_avg
-        self.total_impulse = total_impulse
+        self._total_impulse = total_impulse
         self.burn_time = total_burn_time
-        self.ISP = self.spcific_impulse()
+        self._ISP = self.spcific_impulse()
         pass
+       
+    @property
+    def mass_fuel(self):
+        return self._mass_fuel
     
+    @mass_fuel.setter
+    def mass_fuel(self, new_mass_fuel):
+        self._mass_fuel = new_mass_fuel
+        self._ISP = self.spcific_impulse()
+    
+    @property
+    def total_impulse(self):
+        return self._total_impulse
+    
+    @total_impulse.setter
+    def total_impulse(self, new_total_impulse):
+        self._total_impulse = new_total_impulse
+        self._ISP = self.spcific_impulse()
+        
+    @property
+    def ISP(self):
+        return self._ISP
+
     def spcific_impulse(self) -> float:
-        return self.total_impulse/(self.mass_fuel*abs(self.GRAVITY))
+        return self._total_impulse/(self._mass_fuel*abs(self.GRAVITY))
+
     
     def motor_output(self, t: float) -> tuple [float, float]:
         """Motor_output is the returns the thrust and mass_dot
@@ -66,7 +89,7 @@ class Motor():
             t (float): Simulation time - seconds
 
         Returns:
-            tuple [float, float]: f_thrust - Newtons, mass_dot Kg/second
+            tuple [float, float]: f_thrust - Newtons, mass_dot - Kg/second
         """
                         
         if (t < self.burn_time):
@@ -86,7 +109,7 @@ if __name__ == "__main__":
     drag_coefficient = 0.36 #cf
     diameter = 0.155 # meters
     rocket = RocketConfig(rocket_mass_0, drag_coefficient, diameter)
-    print()
+    
     
     # n Rocket Motor Perameters
     FuelMass = 7512.0/1000.0 # kg
@@ -94,6 +117,11 @@ if __name__ == "__main__":
     TotalImpulse = 14041.0 # Ns
     burn_time = 4.4 #s
     Nmotor = Motor(FuelMass, ThrustAvg, TotalImpulse, burn_time)
-   
+    
+    print(Nmotor.ISP)
+    Nmotor.mass_fuel = 8
+    print(Nmotor.mass_fuel, Nmotor.ISP)
+    Nmotor.total_impulse = 15000.0
+    print(Nmotor.total_impulse, Nmotor.ISP)
    
     #print(Nmotor.motor_output(1), type(Nmotor.motor_output(1)) )
