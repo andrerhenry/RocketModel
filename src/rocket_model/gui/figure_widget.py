46492b6645ea1bd09f9c107c956b84293eb52687
andrerhenry <kinghenry238@gmail.com>
2024-10-09 21:22:06 -0700

    removed old dict

--- src/rocket_model/gui/figure_widget.py
+++ src/rocket_model/gui/figure_widget.py
@@ -49,27 +49,3 @@ class FigureWidget(QtWidgets.QWidget):
         self.axes.set_ylabel(self.data.meta_data[key]["label"])
         self.axes.set_xlabel("Time (s)")
         self.figureCanvas.draw()
-    
-
-        
-def create_data_dict()-> dict:
-    """Generates a dictionary conatianing the associated ploting elements
-    for the attribute that is plotted. 
-    
-    key 1: Labelfor plots
-    key 2: Units for data
-    key 3: Attributes names for the SimulationData class, to select data with key 
-    
-    Returns:
-        dict: figure elements and keys
-    """    
-    data_dict = {
-    "Altitude": {"label": "Altitude (m)", "units": "(m)", "data": "altitude"},
-    "Velocity": {"label": "Velocity (m/s)", "units": "(m/s)", "data": "velocity"},
-    "Acceleration": {"label": "Acceleration (m/s^2)", "units": "(m/s^2)", "data": "acceleration"},
-    "Mass": {"label": "Mass (kg)", "units": "(kg)", "data": "mass"},
-    "Mass_dot":{"label": "Mass_dot (kg/s)", "units": "(kg/s)", "data": "mass_dot"},
-    }
-    
-    return data_dict
-
