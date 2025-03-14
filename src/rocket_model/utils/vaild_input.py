c35ff7eac87f663bff6ef14526e98b95c52d6c96
andrerhenry <kinghenry238@gmail.com>
2025-03-14 00:07:53 -0700

    adding input vaidation fuction

---
+++ src/rocket_model/utils/vaild_input.py
@@ -0,0 +1,11 @@
+
+def validate_positive_number(input: int | float):
+    try:
+        if not isinstance(input, (int, float)):
+            raise TypeError("Input must be an integer or float")
+        if input <= 0:
+            raise ValueError("Input must be a positive number")
+        return input
+    except (TypeError, ValueError) as e:
+        print(f"{type(e).__name__}: {e}")
+
