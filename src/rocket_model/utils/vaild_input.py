c06a771883aa42bde994bf9b085471353d16251c
andrerhenry <kinghenry238@gmail.com>
2025-03-14 22:49:51 -0700

    broke validation function in two parts, vaild and check, add __init__ to module

--- src/rocket_model/utils/vaild_input.py
+++ src/rocket_model/utils/vaild_input.py
@@ -1,14 +1,14 @@
 
 def validate_positive_number(input: int | float):
+    if not isinstance(input, (int, float)):
+        raise TypeError("Input must be an integer or float")
+    if input <= 0:
+        raise ValueError("Input must be a positive number")
+    return input
+
+
+def check_positive_number(input: int | float):
     try:
-        if not isinstance(input, (int, float)):
-            raise TypeError("Input must be an integer or float")
-        if input <= 0:
-            raise ValueError("Input must be a positive number")
-        return input
+        validate_positive_number(input)
     except (TypeError, ValueError) as e:
         print(f"{type(e).__name__}: {e}")
-        return e
-
-if __name__ == "__main__":
-    validate_positive_number(-1)
